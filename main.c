b81f2330fcb31193d1e9b0bfc2e694c3a6a94fb2
Federico Alonso <113862088+FDAB2001@users.noreply.github.com>
2023-10-05 09:26:57 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,2 @@
+// si hay un dado adyacente vertical u horizontal al dado puesto, y si hay 2 o mas dados cerca, se comen los dados y se suman, solo se puede sumar hasta 6, si la 
+//si hay 3 o 4 dados adyacentes que cualquiera sea el numero, el jugador o maquina puee elegir 2 o mas dados para capturar mientras la suma sea menor o igual a 0
