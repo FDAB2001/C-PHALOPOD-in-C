c6f6603e6ed4fe938ac27414ed2967f5e75651d7
Federico Alonso <113862088+FDAB2001@users.noreply.github.com>
2023-10-05 10:54:23 -0300

    Add files via upload

---
+++ computadora.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variables.h"
+
+void computadora(){
+    
+}
---
+++ variables.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+//la primera dimension es la posicion, la segunda es el numero del dado, la tercera es si es del jugador (0) o la maquina (1)
+
+char lamatriz[26][7][2];
+
+int turnoinicial;
