e59bc978c0ee84cb12badf3e46d57e50ec4b318c
lp1-2023 <lp1-2023@lp12023-VirtualBox>
2023-10-05 21:27:49 -0300

    Todo lo que hice hasta ahora pa que funcione

---
+++ TrabajoGrande/src/TPCphalopoid.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variables.h"
+#include "jugador.h"
+#include "computadora.h"
+#include <stdint.h>
+//Funcion que liwmpia el buffer
+
+typedef enum
+{
+	ROJO,
+	VERDE
+}color;
+
+typedef struct
+{
+	uint8_t valorDado;//NUmero del 1 al 6
+	color colorDado; //color del dado
+
+}casilla;
+
+casilla tablero[5][5];
+
+void limpiarBuffer(){
+	int x=0;
+	while((x=getchar())!='\n'&&x!=EOF&&x!=0){}
+}
+
+//Funcion que agrega un enter a la consola
+
+void enter(){
+    printf("\n");
+}
+
+void imprimirMatriz()
+{
+    for (int i = 1; i < 26; i++)
+    {
+        if (lamatriz[i][0][0] != '\0')
+            printf(" %c ", lamatriz[i][0][0]);
+        else
+            printf(" - ");
+        if (i % 5 == 0)
+                    enter();
+    }
+}
+//verifica si el turno que metio el jugador es correcto
+int verificarTurno()
+{
+    int n = 0;
+    while (n != 1 && n != 2)
+        scanf("%d", &n);
+    limpiarBuffer();
+    return n;
+}
+
+int main()
+{
+    int turno = 0;
+    printf("Seleccione quien juega primero \n 1 para jugador primero \n 2 para la computadora primero \n");
+    turnoinicial = verificarTurno();
+    imprimirMatriz();
+            if (turnoinicial == 1)
+                turno=jugador(turno);
+            else//aca tiene que estar la compu
+            	turno=jugador(turno);
+    while (turno != 0)
+    {
+        if (turno==1)
+        	turno=jugador(turno);
+        else //aca tiene que estar la compu pero como no funciona pongo el jugador otv
+        	turno=jugador(turno);
+
+    }
+    return 0;
+}
