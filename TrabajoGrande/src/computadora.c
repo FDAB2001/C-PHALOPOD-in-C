01296919d409c387fc22cab8ea4a9a0a0fac6e5b
RicarditoMilos <146744106+RicarditoMilos@users.noreply.github.com>
2023-10-14 13:48:30 -0300

    Update computadora.c

--- TrabajoGrande/src/computadora.c
+++ TrabajoGrande/src/computadora.c
@@ -1,7 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "variables.h"
+#include <time.h>
 
-void computadora(){
-    
+extern char lamatriz[26][7][2];
+extern int posiocupada[1];
+extern int turnoinicial;
+
+int computadora(turno){
+	printf("hola si paso %d",turnoinicial);
+	turno=1;
+	if(turnoinicial==2){
+		printf("hola si paso");
+		srand(time(NULL));
+		int numerorandom = rand() % 25 + 1;
+		lamatriz[numerorandom][0][0]='1';
+		turnoinicial=1;
+		return(turno);
+	}else{
+
+		if(posiocupada[1]==1){
+			if(lamatriz[2][0][0]=='\0'){
+				lamatriz[2][0][0]='1';
+				return(turno);
+			}
+
+
+				if(lamatriz[6][0][0]=='\0'){
+					lamatriz[6][0][0]='1';
+					return(turno);
+
+				}
+
+			}
+
+		if(posiocupada[1]>1 && posiocupada[1]<5){
+			if(lamatriz[posiocupada[1]-1][0][0]=='\0'){
+				lamatriz[posiocupada[1]-1][0][0]='1';
+				return(turno);
+			}
+			if(lamatriz[posiocupada[1]+1][0][0]=='\0'){
+							lamatriz[posiocupada[1]+1][0][0]='1';
+							return(turno);
+						}
+			if(lamatriz[posiocupada[1]+5][0][0]=='\0'){
+							lamatriz[posiocupada[1]+5][0][0]='1';
+							return(turno);
+						}
+		}
+
+		if(posiocupada[1]==6 || posiocupada[1]==11 || posiocupada[1]==16 ){
+			if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+				lamatriz[posiocupada[1]-5][0][0]='1';
+				return(turno);
+			}
+			if(lamatriz[posiocupada[1]+1][0][0]=='\0'){
+							lamatriz[posiocupada[1]+1][0][0]='1';
+							return(turno);
+						}
+			if(lamatriz[posiocupada[1]+5][0][0]=='\0'){
+							lamatriz[posiocupada[1]+5][0][0]='1';
+							return(turno);
+						}
+
+		}
+
+		if(posiocupada[1]==21){
+			if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+							lamatriz[posiocupada[1]-5][0][0]='1';
+							return(turno);
+						}
+			if(lamatriz[posiocupada[1]+1][0][0]=='\0'){
+							lamatriz[posiocupada[1]+1][0][0]='1';
+							return(turno);
+						}
+		}
+
+		if(posiocupada[1]==25){
+					if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+									lamatriz[posiocupada[1]-5][0][0]='1';
+									return(turno);
+								}
+					if(lamatriz[posiocupada[1]-1][0][0]=='\0'){
+									lamatriz[posiocupada[1]-1][0][0]='1';
+									return(turno);
+								}
+				}
+
+
+		if(posiocupada[1]>21 && posiocupada[1]<25){
+			if(lamatriz[posiocupada[1]-1][0][0]=='\0'){
+				lamatriz[posiocupada[1]-1][0][0]='1';
+				return(turno);
+			}
+			if(lamatriz[posiocupada[1]+1][0][0]=='\0'){
+							lamatriz[posiocupada[1]+1][0][0]='1';
+							return(turno);
+						}
+			if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+							lamatriz[posiocupada[1]-5][0][0]='1';
+							return(turno);
+						}
+		}
+
+		if(posiocupada[1]==10 || posiocupada[1]==15 || posiocupada[1]==20 ){
+					if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+						lamatriz[posiocupada[1]-5][0][0]='1';
+						return(turno);
+					}
+					if(lamatriz[posiocupada[1]-1][0][0]=='\0'){
+									lamatriz[posiocupada[1]-1][0][0]='1';
+									return(turno);
+								}
+					if(lamatriz[posiocupada[1]+5][0][0]=='\0'){
+									lamatriz[posiocupada[1]+5][0][0]='1';
+									return(turno);
+								}
+
+				}
+
+
+		if((posiocupada[1]>6 && posiocupada[1]<10) || (posiocupada[1]>11 && posiocupada[1]<15) || (posiocupada[1]>16 && posiocupada[1]<20)){
+			if(lamatriz[posiocupada[1]+1][0][0]=='\0'){
+										lamatriz[posiocupada[1]+1][0][0]='1';
+										return(turno);
+									}
+						if(lamatriz[posiocupada[1]-5][0][0]=='\0'){
+										lamatriz[posiocupada[1]-5][0][0]='1';
+										return(turno);
+									}
+						if(lamatriz[posiocupada[1]+5][0][0]=='\0'){
+								lamatriz[posiocupada[1]+5][0][0]='1';
+								return(turno);
+															}
+						if(lamatriz[posiocupada[1]-1][0][0]=='\0'){
+						lamatriz[posiocupada[1]-1][0][0]='1';
+						return(turno);
+															}
+		}
+
+	}
+
+return(turno);
+
+
 }
