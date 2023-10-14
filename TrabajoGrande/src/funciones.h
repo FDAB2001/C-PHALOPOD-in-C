de9c7f6c7265676197a1fe8191fb806cd69432bf
Federico Alonso <113862088+FDAB2001@users.noreply.github.com>
2023-10-14 16:09:22 -0300

    Create funciones.h

---
+++ TrabajoGrande/src/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+void limpiarBuffer();
+void enter();
+void imprimirMatriz();
+int verificarTurno();
+void tablaPosiciones();
+
+#endif /* FUNCIONES_H_ */
