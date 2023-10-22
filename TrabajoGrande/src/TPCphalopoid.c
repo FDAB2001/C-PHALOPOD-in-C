aaa9afb90dcf6d5d145fa227c844e46ba918db44
Federico Alonso <113862088+FDAB2001@users.noreply.github.com>
2023-10-22 18:12:29 -0300

    Update TPCphalopoid.c

--- TrabajoGrande/src/TPCphalopoid.c
+++ TrabajoGrande/src/TPCphalopoid.c
@@ -4,7 +4,7 @@
 #include "jugador.h"
 #include "computadora.h"
 #include <stdint.h>
-//Funcion que liwmpia el buffer
+
 
 typedef enum
 {
@@ -21,56 +21,57 @@ typedef struct
 
 casilla tablero[5][5];
 
-void limpiarBuffer(){
-	int x=0;
-	while((x=getchar())!='\n'&&x!=EOF&&x!=0){}
+
+
+void sumarCostado(int posicion){
+
 }
 
-//Funcion que agrega un enter a la consola
+void verificarGanador(int *turno){
+	int cont=0;
+	int jug=0;
+	int AI=0;
+	for(int i=1;i<26;i++){
+		for(int j=1;j<7;j++){
+			if(lamatriz[i][j][0]=='1'){
+				jug++;
+				cont++;
+			}else if(lamatriz[i][j][0]=='2'){
+				AI++;
+				cont++;
+			}
+		}
+	}if(cont==25){
+		*turno=0;
+		if(AI<jug)
+			printf("Gana el jugador");
+		else printf("Gana LA MAQUINARIA");
+	}
 
-void enter(){
-    printf("\n");
-}
 
-void imprimirMatriz()
-{
-    for (int i = 1; i < 26; i++)
-    {
-        if (lamatriz[i][0][0] != '\0')
-            printf(" %d ", lamatriz[i][0][0]);
-        else
-            printf(" - ");
-        if (i % 5 == 0)
-                    enter();
-    }
 }
-//verifica si el turno que metio el jugador es correcto
-int verificarTurno()
-{
-    int n = 0;
-    while (n != 1 && n != 2)
-        scanf("%d", &n);
-    limpiarBuffer();
-    return n;
-}
-
 int main()
 {
     int turno = 0;
     printf("Seleccione quien juega primero \n 1 para jugador primero \n 2 para la computadora primero \n");
     turnoinicial = verificarTurno();
+    tablaPosiciones();
     imprimirMatriz();
             if (turnoinicial == 1)
                 turno=jugador(turno);
+
             else//aca tiene que estar la compu
-            	turno=jugador(turno);
+            	turno=computadora(turno);
     while (turno != 0)
-    {
+    { 	printf("Tablero actual: \n");
+		imprimirMatriz();
         if (turno==1)
         	turno=jugador(turno);
-        else //aca tiene que estar la compu pero como no funciona pongo el jugador otv
-        	turno=jugador(turno);
-
+        else //aca tiene que estar la compu
+        	turno=computadora(turno);
+        verificarGanador(&turno);
     }
+    enter();
+    printf("fin");
     return 0;
 }
