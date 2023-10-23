c5ddbf9876b3cd1c2e1c75dd6df573e56314f39a
Federico Alonso <113862088+FDAB2001@users.noreply.github.com>
2023-10-23 00:12:19 -0300

    Update jugador.h

--- TrabajoGrande/src/jugador.h
+++ TrabajoGrande/src/jugador.h
@@ -3,8 +3,8 @@
 
 //SOLO PROTOTIPO DE FUNCIONES Y VARIABLES
 void dadoUno(int);
+void sumarDado(int,int);
 int validarJugada(void);
 int jugador(int);
 void ponerDadojugador(int);
-
 #endif /* JUGADOR_H_ */
