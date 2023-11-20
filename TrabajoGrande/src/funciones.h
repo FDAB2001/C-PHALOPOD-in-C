6c5daad4527c009b1df113745ec270516b986440
RicarditoMilos <146744106+RicarditoMilos@users.noreply.github.com>
2023-11-20 00:04:48 -0300

    Update funciones.h

--- TrabajoGrande/src/funciones.h
+++ TrabajoGrande/src/funciones.h
@@ -2,10 +2,14 @@
 #define FUNCIONES_H_
 
 void limpiarBuffer();
+void vaciar_matriz();
 void enter();
 void imprimirMatriz();
-int verificarTurno();
+int verificarTurno(int);
 void tablaPosiciones();
 void imprimirDecision();
+void decirturnoini(int);
+void decirparente();
+int numeroAleatorio(int,int);
 
 #endif /* FUNCIONES_H_ */
