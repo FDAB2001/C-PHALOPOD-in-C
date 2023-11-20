e10d74b219db3379592490b990227415ae8e6d76
RicarditoMilos <146744106+RicarditoMilos@users.noreply.github.com>
2023-11-20 00:06:34 -0300

    Update variables.h

--- TrabajoGrande/src/variables.h
+++ TrabajoGrande/src/variables.h
@@ -2,10 +2,17 @@
 #define VARIABLES_H_
 //la primera dimension es la posicion, la segunda es el numero del dado, la tercera es si es del jugador (0) o la maquina (1)
 
-
-int lamatriz[26][7][2];
+#include <gtk/gtk.h>
 int posiocupada[1];
+int lamatriz[26][7][2];
 //de quien es el turno inicial
 int turnoinicial;
-
+int decisiontomada;
+int turno = 0;
+int turnoinicial;
+int decisionturno;
+int jugar_pressed;
+int turnoinicial;
+int jugadaGTK;
+int modo=0;
 #endif //VARIABLES_H_
