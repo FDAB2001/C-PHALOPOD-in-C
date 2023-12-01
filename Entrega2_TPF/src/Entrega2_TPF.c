4643f455a722952672fba1638e406b0d545527a0
RicarditoMilos <146744106+RicarditoMilos@users.noreply.github.com>
2023-12-01 02:14:23 -0300

    Update Entrega2_TPF.c

--- Entrega2_TPF/src/Entrega2_TPF.c
+++ Entrega2_TPF/src/Entrega2_TPF.c
@@ -4,6 +4,7 @@
 #include "variables.h"
 #include "jugador.h"
 #include "computadora.h"
+#include "funciones.h"
 #include <stdint.h>
 #define GET_WIDGET(builder, name) GTK_WIDGET(gtk_builder_get_object(builder, name))
 //el filepath del glade para rapido acceso y uso
@@ -13,16 +14,14 @@
 extern int jugadaGTK;
 extern int lamatriz[26][7][2];
 extern int decisiontomada;
-void vaciar_matriz();
-int numeroAleatorio();
-void imprimir_matriz();
-void actualizar_GTK();
 const gchar *nombre_jugador = NULL;
 const gchar *nombre_jugador2 = NULL;
 extern int jugar_pressed;
 extern int modo;
 extern int suma;
 extern int vector[5];
+extern char nombrejugador1Global[50];//nombre del jugador 1 actual que se guardara en un archivo
+extern char nombrejugador2Global[50];//nombre del jugador 2 actual que se guardara en un archivo
 int temp1;
 int temp2;
 int temp3;
@@ -113,163 +112,6 @@ void declarar_widgets() {
 	BOTON25 = GET_WIDGET(constructor, "menu_tablero_boton_25");
 }
 
-//1 jcj 2 jcia 3 iacia
-
-//Las siguientes 6 funciones togglean el boton seleccionado al mismo tiempo que destoglea el resto de botones
-//En el menu opciones, los primeros 3 botones toggleables
-void TOGLEADO_JCJ(GtkToggleButton *widget, gpointer data) {
-	if (gtk_toggle_button_get_active(widget)) {
-		// Botón JCJ activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_JCIA), FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_iacia), FALSE);
-		modo=1;
-	}
-}
-
-void TOGLEADO_JCIA(GtkToggleButton *widget, gpointer data) {
-	if (gtk_toggle_button_get_active(widget)) {
-		// Botón JUGADOR CONTRA IA activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_jcj), FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_iacia), FALSE);
-		modo=2;
-	}
-}
-void TOGLEADO_IACIA(GtkToggleButton *widget, gpointer data) {
-	if (gtk_toggle_button_get_active(widget)) {
-		// Botón IACIA activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_JCIA), FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_jcj), FALSE);
-		modo=3;
-	}
-}
-//En el menu opciones, los segundos 3 botones toggleables
-void TOGLEADO_PAR(GtkToggleButton *toggle_button, gpointer data) {
-	if (gtk_toggle_button_get_active(toggle_button)) {
-		// Botón PARENTESIS activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_corchetes),
-				FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_aleatorio),
-				FALSE);
-		decisiontomada=1;//cuando decisiontomada==1, el usuario quiere ser parentesis, compu será corchete
-	}
-}
-void TOGLEADO_COR(GtkToggleButton *toggle_button, gpointer data) {
-	if (gtk_toggle_button_get_active(toggle_button)) {
-		// Botón CORCHETES activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_parentesis),
-				FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_aleatorio),
-				FALSE);
-		decisiontomada=2;//cuando decisiontomada==2, el usuario quiere ser corchete, compu será parentesis
-
-
-
-	}
-}
-
-void TOGLEADO_ALE(GtkToggleButton *toggle_button, gpointer data) {
-	if (gtk_toggle_button_get_active(toggle_button)) {
-		// Botón ALEATORIO activado
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_corchetes),
-				FALSE);
-		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_parentesis),
-				FALSE);
-		decisiontomada= numeroAleatorio(2, 1);
-	}
-}
-
-//Funcion del tablero
-void ABRIR_TABLERO(GtkWidget *widget, gpointer data) {
-	if (decisiontomada!=0 && modo!=0) {
-		if(decisiontomada==2){
-		computadora(1);
-		actualizar_GTK();
-		}
-		jugar_pressed = 0; // Así el loop termina cuando se presiona salir
-
-		// Obtén el nombre del jugador del widget de entrada de texto
-		// desde aquí se manejan los nombres de los jugadores si se seleccionó 1
-		const gchar *nombre = gtk_entry_get_text(GTK_ENTRY(opciones_nombre));
-		const gchar *nombre2 = gtk_entry_get_text(GTK_ENTRY(opciones_nombre2));
-
-		if (modo == 1) {
-			// Verifica que el nombre no esté vacío antes de asignarlo
-			if (nombre2 != NULL && g_strcmp0(nombre2, "") != 0) {
-				// Limita la longitud del nombre para evitar desbordamientos
-				const gsize max_length2 = 50;  // Establece la longitud
-				gchar nombre_limitado2[max_length2 + 1];  // +1 para el carácter nulo
-				g_strlcpy(nombre_limitado2, nombre2, max_length2 + 1);
-
-				nombre_jugador2 = g_strdup(nombre_limitado2); // Copia el nombre limitado
-			} else {
-				// Proporciona un valor predeterminado
-				nombre_jugador2 = "La Computadora";
-			}
-		}
-
-		// Verifica que el nombre no esté vacío antes de asignarlo
-		if (nombre != NULL && g_strcmp0(nombre, "") != 0) {
-			// Limita la longitud del nombre para evitar desbordamientos
-			const gsize max_length = 50;  // Establece la longitud
-			gchar nombre_limitado[max_length + 1];  // +1 para el carácter nulo
-			g_strlcpy(nombre_limitado, nombre, max_length + 1);
-
-			nombre_jugador = g_strdup(nombre_limitado);  // Copia el nombre limitado
-		} else {
-			// Proporciona un valor predeterminado
-			nombre_jugador = "Jugador Anónimo";
-		}
-
-		// Muestra la ventana del tablero
-		gtk_widget_show(menu_tablero);
-		gtk_widget_hide(menu_opciones);
-	}
-}
-
-void EXPOSICION_NOMBRE(GtkWidget *widget, gpointer data) {
-    // Verifica si hay un nombre de jugador para mostrar
-    // Agregar aquí los errores, si no hay error, mostrar el nombre, si no, mostrar el error
-    if (modo == 1 && nombre_jugador2 != NULL) {
-        // Establece el texto en el widget de visualización de nombre2
-        gtk_label_set_text(GTK_LABEL(tablero_nombre_display2), nombre_jugador2);
-	gtk_label_set_text(GTK_LABEL(tablero_nombre_display), nombre_jugador);
-    } else if (nombre_jugador != NULL) {
-        // Establece el texto en el widget de visualización de nombre
-        gtk_label_set_text(GTK_LABEL(tablero_nombre_display), nombre_jugador);
-    } else {
-        // Maneja el caso donde no hay nombre de jugador
-        g_print("Nombre no especificado\n");
-    }
-}
-
-void MOSTRAR_OPCIONES(GtkWidget *widget, gpointer data) {
-	gtk_widget_show(menu_opciones);
-}
-
-//Mostrar y ocultar el menu de como jugar
-void MOSTRAR_INSTRUCCIONES(GtkWidget *widget, gpointer data) {
-	gtk_widget_show(menu_como_jugar);
-}
-void OCULTAR_INSTRUCCIONES(GtkWidget *widget, gpointer data) {
-	gtk_widget_hide(menu_como_jugar);
-}
-
-//Mostrar y ocultar el menu de creditos
-void MOSTRAR_CREDITOS(GtkWidget *widget, gpointer data) {
-	gtk_widget_show(menu_creadores);
-}
-void OCULTAR_CREDITOS(GtkWidget *widget, gpointer data) {
-	gtk_widget_hide(menu_creadores);
-}
-void verificarjugada(int jugada) {
-
-
-	if (lamatriz[jugada][0][0] == '\0') {
-		ponerDadojugador(jugada);
-		posiocupada[1] = jugadaGTK;
-		computadora(1);
-	}
-}
 void actualizar_GTK() { //como dice la funcion, el GTK "mira" la matriz y se actualiza en torno a ella
 	for (int i = 1; i < 26; ++i) {
 		int valor = lamatriz[i][0][0];
@@ -473,6 +315,199 @@ void actualizar_GTK() { //como dice la funcion, el GTK "mira" la matriz y se act
 		}
 	}
 }
+//1 jcj 2 jcia 3 iacia
+
+//Las siguientes 6 funciones togglean el boton seleccionado al mismo tiempo que destoglea el resto de botones
+//En el menu opciones, los primeros 3 botones toggleables
+void TOGLEADO_JCJ(GtkToggleButton *widget, gpointer data) {
+	if (gtk_toggle_button_get_active(widget)) {
+		// Botón JCJ activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_JCIA), FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_iacia), FALSE);
+		modo=1;
+	}
+}
+
+void TOGLEADO_JCIA(GtkToggleButton *widget, gpointer data) {
+	if (gtk_toggle_button_get_active(widget)) {
+		// Botón JUGADOR CONTRA IA activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_jcj), FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_iacia), FALSE);
+		modo=2;
+	}
+}
+void TOGLEADO_IACIA(GtkToggleButton *widget, gpointer data) {
+	if (gtk_toggle_button_get_active(widget)) {
+		// Botón IACIA activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_JCIA), FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_jcj), FALSE);
+		modo=3;
+	}
+}
+
+//En el menu opciones, los segundos 3 botones toggleables
+
+void TOGLEADO_PAR(GtkToggleButton *toggle_button, gpointer data) {
+	if (gtk_toggle_button_get_active(toggle_button)) {
+		// Botón PARENTESIS activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_corchetes),
+				FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_aleatorio),
+				FALSE);
+		decisiontomada=1;//cuando decisiontomada==1, el usuario quiere ser parentesis, compu será corchete
+		turno=1;
+	}
+}
+void TOGLEADO_COR(GtkToggleButton *toggle_button, gpointer data) {
+	if (gtk_toggle_button_get_active(toggle_button)) {
+		// Botón CORCHETES activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_parentesis),
+				FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_aleatorio),
+				FALSE);
+		decisiontomada=2;//cuando decisiontomada==2, el usuario quiere ser corchete, compu será parentesis
+		turno=2;
+
+
+	}
+}
+
+void TOGLEADO_ALE(GtkToggleButton *toggle_button, gpointer data) {
+	if (gtk_toggle_button_get_active(toggle_button)) {
+		// Botón ALEATORIO activado
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_corchetes),
+				FALSE);
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(opciones_parentesis),
+				FALSE);
+		decisiontomada= numeroAleatorio(2, 1);
+		turno=decisiontomada;
+	}
+}
+
+//Funcion del tablero
+
+void ABRIR_TABLERO(GtkWidget *widget, gpointer data) {
+	if (decisiontomada != 0 && modo != 0) {
+		if (decisiontomada == 2) {
+			computadora(1);
+			actualizar_GTK();
+		}
+		jugar_pressed = 0; // Así el loop termina cuando se presiona salir
+
+		// Obtén el nombre del jugador del widget de entrada de texto
+		// desde aquí se manejan los nombres de los jugadores si se seleccionó 1
+		const gchar *nombre = gtk_entry_get_text(GTK_ENTRY(opciones_nombre));
+		const gchar *nombre2 = gtk_entry_get_text(GTK_ENTRY(opciones_nombre2));
+
+		if (modo == 1) {
+			// Verifica que el nombre no esté vacío antes de asignarlo
+			if (nombre2 != NULL && g_strcmp0(nombre2, "") != 0) {
+				// Limita la longitud del nombre para evitar desbordamientos
+				const gsize max_length2 = 50;  // Establece la longitud
+				gchar nombre_limitado2[max_length2 + 1]; // +1 para el carácter nulo
+				g_strlcpy(nombre_limitado2, nombre2, max_length2 + 1);
+				nombre_jugador2 = g_strdup(nombre_limitado2); // Copia el nombre limitado
+				strncpy(nombrejugador2Global,nombre2,sizeof(nombrejugador2Global));
+				printf("%s",nombrejugador2Global);
+			} else {
+				// Proporciona un valor predeterminado
+				nombre_jugador2 = "Jugador Anonimo 2";
+			}
+		}else if(modo ==2){nombre_jugador2 = "La Computadora";
+		strncpy(nombrejugador2Global,nombre_jugador2,sizeof(nombrejugador2Global));}
+
+		// Verifica que el nombre no esté vacío antes de asignarlo
+		if (nombre != NULL && g_strcmp0(nombre, "") != 0) {
+			// Limita la longitud del nombre para evitar desbordamientos
+			const gsize max_length = 50;  // Establece la longitud
+			gchar nombre_limitado[max_length + 1];  // +1 para el carácter nulo
+			g_strlcpy(nombre_limitado, nombre, max_length + 1);
+			nombre_jugador = g_strdup(nombre_limitado); // Copia el nombre limitado
+			strncpy(nombrejugador1Global,nombre,sizeof(nombrejugador1Global));//Copia el nombre a la variable global
+			printf("%s",nombrejugador1Global);//Copia el nombre a la variable global
+		} else {
+			// Proporciona un valor predeterminado
+			nombre_jugador = "Jugador Anónimo";
+			strncpy(nombrejugador1Global,nombre_jugador,sizeof(nombrejugador1Global));
+		}
+
+		// Muestra la ventana del tablero
+		gtk_widget_show(menu_tablero);
+		gtk_widget_hide(menu_opciones);
+	}
+}
+
+void EXPOSICION_NOMBRE(GtkWidget *widget, gpointer data) {
+    // Verifica si hay un nombre de jugador para mostrar
+    // Agregar aquí los errores, si no hay error, mostrar el nombre, si no, mostrar el error
+    if (modo == 1 && nombre_jugador2 != NULL) {
+        // Establece el texto en el widget de visualización de nombre2
+        gtk_label_set_text(GTK_LABEL(tablero_nombre_display2), nombre_jugador2);
+	gtk_label_set_text(GTK_LABEL(tablero_nombre_display), nombre_jugador);
+    } else if (nombre_jugador != NULL) {
+        // Establece el texto en el widget de visualización de nombre
+        gtk_label_set_text(GTK_LABEL(tablero_nombre_display), nombre_jugador);
+    } else {
+        // Maneja el caso donde no hay nombre de jugador
+        g_print("Nombre no especificado\n");
+    }
+}
+
+void MOSTRAR_OPCIONES(GtkWidget *widget, gpointer data) {
+	gtk_widget_show(menu_opciones);
+}
+
+//Mostrar y ocultar el menu de como jugar
+void MOSTRAR_INSTRUCCIONES(GtkWidget *widget, gpointer data) {
+	gtk_widget_show(menu_como_jugar);
+}
+void OCULTAR_INSTRUCCIONES(GtkWidget *widget, gpointer data) {
+	gtk_widget_hide(menu_como_jugar);
+}
+
+//Mostrar y ocultar el menu de creditos
+void MOSTRAR_CREDITOS(GtkWidget *widget, gpointer data) {
+	gtk_widget_show(menu_creadores);
+}
+void OCULTAR_CREDITOS(GtkWidget *widget, gpointer data) {
+	gtk_widget_hide(menu_creadores);
+}
+
+void verificarjugada(int jugada) {
+
+	if (lamatriz[jugada][0][0] == '\0' && turno != 0) {
+		if(modo==1&&decisiontomada==1){
+			turno = jugador(turno, jugada);
+			decisiontomada=2;
+			printf("entra primero");
+			enter();
+		}
+		else if(modo==1&&decisiontomada==2){
+			turno=jugador(turno,jugada);
+			decisiontomada=1;
+			printf("entra segundo");
+			enter();
+		}
+		if(modo==2){
+		turno = jugador(turno, jugada);
+		int posicvacia=0;
+		for (int i = 1; i < 26; i++) {
+			//antes del turno de la computadora se verifica que existen lugares disponibles por si acaso
+			if (lamatriz[i][0][0] == 0 || lamatriz[i][0][0] == '\0') {
+				posicvacia++;
+			}
+		}
+		if (posicvacia != 0) {
+			posiocupada[1] = jugadaGTK;
+			turno = computadora(1);
+		}
+		verificarGanador(modo);
+
+		}
+	}
+
+}
+
 
 void PRESIONADO(GtkWidget *widget, gpointer data) {//funcion que se llama al presionar cualquier boton
 
@@ -489,8 +524,9 @@ void REINICIAR(GtkWidget *widget, gpointer data) {
     // Actualiza la interfaz gráfica
     actualizar_GTK();
 }
+int botonestogleadoscapturamultiple;
 void OCULTAR_SELECCIONAR(GtkWidget *widget, gpointer data) {
-	if (suma < 7 && suma > 2) {
+	if (suma < 7  && suma > 1&&botonestogleadoscapturamultiple>1) {
 		sumarDado(suma, jugadaGTK);
 		actualizar_GTK();
 		gtk_widget_show(menu_tablero);
@@ -525,6 +561,7 @@ void BOTONES_TOGGLE(GtkToggleButton *toggle_button, gpointer data) {
 	char buffer[20];
 	int num = GPOINTER_TO_INT(data);
 	if (gtk_toggle_button_get_active(toggle_button)) {
+		botonestogleadoscapturamultiple++;
 		switch (num) {
 		case 0:
 			temp1 = lamatriz[jugadaGTK - 1][0][0];
@@ -548,6 +585,7 @@ void BOTONES_TOGGLE(GtkToggleButton *toggle_button, gpointer data) {
 		snprintf(buffer, sizeof(buffer), "%d", suma);
 		gtk_label_set_text(GTK_LABEL(suma_display), buffer);
 	} else {
+		botonestogleadoscapturamultiple--;
 		switch (num) {
 		case 0:
 			lamatriz[jugadaGTK - 1][0][0] = temp1;
